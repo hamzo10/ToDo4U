fe569681171c1706183b94e51a98f75d72f43073
Luca Feigl <luca.feigl@ion2s.com>
2019-10-21 11:00:00 +0200

    Add TODOs

--- server/api/index.js
+++ server/api/index.js
@@ -3,10 +3,14 @@ const toDo = require('./toDo');
 
 const router = Router();
 
+// TODO: Split api / add info api-endpoint
+
 router.post('/new', toDo.new);
 
 router.get('/all', toDo.all);
 
 router.patch('/update/:id', toDo.update);
+
+// TODO: Add delete todo api-endpoint
 
 module.exports = router;
