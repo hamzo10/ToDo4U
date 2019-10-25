55958f2d7ae4a1b2379fecdba4a7160dcc92635e
hamzahqazi <hamzah.qazi@ion2s.com>
2019-10-25 10:45:55 +0200

    Improve api routes

--- server/api/index.js
+++ server/api/index.js
@@ -6,12 +6,8 @@ const router = Router();
 // TODO: Split api / add info api-endpoint
 
 router.post('/new', toDo.new);
-
 router.get('/all', toDo.all);
-
-router.patch('/update/:id', toDo.update);
-
-// TODO: Add delete todo api-endpoint
-router.delete('/delete/:id', toDo.delete);
+router.patch('/:id', toDo.update);
+router.delete('/:id', toDo.delete);
 
 module.exports = router;
