d609516acd9d7011f0148ec7af7b0e39e1a83c4b
Luca Feigl <luca.feigl@ion2s.com>
2020-05-15 16:02:36 +0200

    Fix linting errors

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
     </v-container>
 </template>
 
