8afb229d47d82f5faf4552d84ec85e2e79577225
Luca Feigl <luca.feigl@ion2s.com>
2020-05-15 15:55:09 +0200

    Re-add router view

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,59 +2,27 @@
     <v-app>
         <v-app-bar
             app
-            color="primary"
+            color="blue"
             dark
         >
-            <div class="d-flex align-center">
-                <v-img
-                    alt="Vuetify Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                    transition="scale-transition"
-                    width="40"
-                />
-
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
+            <div id="nav">
+                <router-link to="/">
+                    Home
+                </router-link> |
+                <router-link to="/about">
+                    About
+                </router-link>
             </div>
-
-            <v-spacer />
-
-            <v-btn
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-                text
-            >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
         </v-app-bar>
 
         <v-content>
-            <HelloWorld />
+            <router-view />
         </v-content>
     </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
     name: 'App',
-
-    components: {
-        HelloWorld,
-    },
-
-    data: () => ({
-    //
-    }),
 };
 </script>
