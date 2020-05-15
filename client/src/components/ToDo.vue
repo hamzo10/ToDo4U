4fd04fc59d0d7ce94dbe357f998f744e53be2ffd
Hamzah Qazi <hamzah.qazi@ion2s.com>
2020-05-15 16:33:08 +0200

    Create ToDo Component

---
+++ client/src/components/ToDo.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container>
+      <v-card>
+          <v-card-title>
+              Your ToDo:
+          </v-card-title>
+          <v-card-subtitle>
+              <v-chip>new</v-chip>
+              <v-chip>2020</v-chip>
+              <v-chip>Corona</v-chip>
+          </v-card-subtitle>
+          <v-card-text>
+                {{msg}}
+          </v-card-text>
+          <v-card-actions>
+
+          </v-card-actions>
+      </v-card>
+
+    </v-container>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+    name: 'ToDo',
+    props: [
+        'msg',
+    ],
+};
+</script>
